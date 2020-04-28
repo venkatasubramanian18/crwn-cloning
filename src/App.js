406a31f14fc9343e1027b085003f4d717e72b758
Venkatasubramanian Subburaman <venkatasubramanian18@gmail.com>
2020-04-29 03:00:42 +0530

    adding heading component and shop page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import {Switch, Route} from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import HomePage from './page/homepage/homepage.component'
 import ShopPage from './page/shop/shop.component';
 
@@ -10,6 +11,7 @@ import ShopPage from './page/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
